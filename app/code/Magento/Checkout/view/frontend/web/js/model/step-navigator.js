108c3fac3d5bde3353433199359582e7bf7cea0f
Kapil Chauhan <kapil.kumar@incaendo.com>
2018-01-24 13:21:11 +0530

    Resolving address blank issue of 2.1 for #11197
    Resolving address blank issue of 2.1 for #11197

--- app/code/Magento/Checkout/view/frontend/web/js/model/step-navigator.js
+++ app/code/Magento/Checkout/view/frontend/web/js/model/step-navigator.js
@@ -135,7 +135,8 @@ define(
                 if (steps().length > activeIndex + 1) {
                     var code = steps()[activeIndex + 1].code;
                     steps()[activeIndex + 1].isVisible(true);
-                    window.location = window.checkoutConfig.checkoutUrl + "#" + code;
+                    //window.location = window.checkoutConfig.checkoutUrl + "#" + code;                    
+                    window.location.hash = code;
                     document.body.scrollTop = document.documentElement.scrollTop = 0;
                 }
             }
